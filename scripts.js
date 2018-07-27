ea8a4499a074577b8c402ce96e836ce93724d0f9
zeyuri <gipsyonacid@gmail.com>
2018-07-27 00:28:33 -0300

    first exercise before correction

--- scripts.js
+++ scripts.js
@@ -1,3 +1,22 @@
 //Use this file to implement Part One of your project
+var animal = {};
 
+animal.username = 'Rast';
+animal['tagline'] = 'Come on tiger';
 
+var noises = [];
+
+animal['noises'] = noises
+
+var counter = 0;
+
+for(var key in animal) {
+    counter++;
+    if (key == 'username') {
+        console.log('Hi my name is ' + animal[key]);
+    }
+    if (key == 'tagline') {
+        console.log('I like to say ' + animal[key]);
+    }
+    
+}
