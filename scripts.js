04447730abe73fda4813fb49e038f9dee4ce012d
zeyuri <gipsyonacid@gmail.com>
2018-07-27 00:28:33 -0300

    arrayu exercise before correction

--- scripts.js
+++ scripts.js
@@ -1,22 +1,69 @@
-//Use this file to implement Part One of your project
-var animal = {};
+var animal = {}; 
+ 
+animal.username = 'Rast'; 
+animal['tagline'] = 'Come on tiger'; 
+ 
+var noises = []; 
+animal['noises'] = noises 
+ 
+console.log(animal);
 
-animal.username = 'Rast';
-animal['tagline'] = 'Come on tiger';
-
-var noises = [];
-
-animal['noises'] = noises
-
-var counter = 0;
-
-for(var key in animal) {
-    counter++;
-    if (key == 'username') {
-        console.log('Hi my name is ' + animal[key]);
-    }
-    if (key == 'tagline') {
-        console.log('I like to say ' + animal[key]);
-    }
-    
+var counter = 0; 
+ 
+for(var key in animal) { 
+    counter++; 
+    if (key == 'username') { 
+        console.log('Hi my name is ' + animal[key]); 
+    } else if (key == 'tagline') { 
+        console.log('I like to say ' + animal[key]); 
+    } 
+     
 }
+
+var noiseArray = ['cocorico'];
+
+noiseArray.unshift('latido');
+noiseArray.push('miaou');
+noiseArray[3] = 'mugido';
+
+animal.noises = noiseArray;
+
+var animals = [];
+
+animals[0] = animal;
+
+var quackers = { 
+    username: 'DaffyDuck',
+     tagline: 'Yippeee!',
+      noises: [
+            'quack',
+            'honk',
+            'sneeze',
+            'growl'
+        ] 
+    };
+
+var felino = { 
+    username: 'Onça Pintada',
+        tagline: 'Aqui tem coragem',
+        noises: [
+            'rawwwr',
+            'MEAOW',
+        ] 
+    };
+
+var cerradeiro = { 
+    username: 'Mico',
+        tagline: 'Sou bonito',
+        noises: [
+            'Mu mu aaaa',
+            'Uh Uh Uh Uh',
+        ] 
+    };
+    
+animals.push(quackers);
+console.log(animals);
+animals.push(felino);
+animals.push(cerradeiro);
+
+console.log(animals);
